98bf63029ffd32c4693b9b680a3a9f5a1af48f82
k3vinrich4rd <kevinrichardve@gmail.com>
2025-02-03 12:43:28 -0300

    Implementando a criacao de uma nova classe e ajuste de escopo

---
+++ src/Filme.java
@@ -0,0 +1,13 @@
+public class Filme {
+    public static void main(String[] args) {
+
+        System.out.println("Esse é screen match");
+        System.out.println("Filme: Top Gun Maverick ");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean estaNoPlano = true;
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do filme: " + notaDoFilme);
+    }
+}
--- src/primeirodesafio/Estudos.java
+++ src/primeirodesafio/Estudos.java
@@ -2,6 +2,7 @@ package primeirodesafio;
 
 public class Estudos {
     public static void main(String[] args) {
+
         System.out.println("""
                 Anotações de estudos
                 Tema: Dicas para Redação

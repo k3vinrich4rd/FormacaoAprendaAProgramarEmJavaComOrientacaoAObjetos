de3306c6f81897212496c672068158d12d3196b4
k3vinrich4rd <kevinrichardve@gmail.com>
2025-02-12 11:48:32 -0300

    Implementando resolucao de desafio proposto

---
+++ src/collectionselistas/project/segundodesafio/ProdutoPerecivel.java
@@ -0,0 +1,22 @@
+package collectionselistas.project.segundodesafio;
+
+public class ProdutoPerecivel extends Produto {
+
+    String dataValidade;
+
+    public ProdutoPerecivel(String nome, double preco, int quantidade, String dataValidade) {
+        super(nome, preco, quantidade);
+        this.dataValidade = dataValidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto Perecível: " + nome +
+                ", Data de validade: " + dataValidade +
+                ", Preco: " + preco +
+                " e Quantidade: " + quantidade;
+    }
+}
+
+
+

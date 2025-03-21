9818832826c6b4168ffa68f5e44d6b145be682f9
k3vinrich4rd <kevinrichardve@gmail.com>
2025-03-21 11:56:30 -0300

    Implementando a resolucao do terceiro desafio

---
+++ src/poo/screenmatch/terceirodesafio/numerosprimos/GeradorPrimo.java
@@ -0,0 +1,15 @@
+
+
+
+
+package poo.screenmatch.terceirodesafio.numerosprimos;
+
+public class GeradorPrimo extends NumerosPrimos {
+    public int gerarProximoPrimo(int ultimoPrimoConhecido) {
+        int proximoNumero = ultimoPrimoConhecido + 1;
+        while (!verificarPrimalidade(proximoNumero)) {
+            proximoNumero++;
+        }
+        return proximoNumero;
+    }
+}
